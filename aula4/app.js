9be8acbb6f4ad39c98af7d356c938d1247cd6ffe
Maria Eduarda Alves da Silva <maria.silva@bndes.gov.br>
2024-09-06 13:55:20 -0300

    Add aula4

---
+++ aula4/app.js
@@ -0,0 +1,16 @@
+// console.log(dados);
+
+let section = document.getElementById("resultados-pesquisa");
+
+section.innerHTML = `
+    <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">Haikyuu</a>
+                </h2>
+                <p class="descricao-meta">
+                    5 estrelas. Esporte, slice of life. Envolvente e emocionante, depois de começar nã consegue largar. Impossível não se apegar a todos os personagens.
+                </p>
+                <a href="https://www.crunchyroll.com/pt-br/series/GY8VM8MWY/haikyu?srsltid=AfmBOorrtSSBRB_FT-7L0HR5SFt8-7vBjhSVlxnKfXmr4SMQvUMDo26Y"
+                    target="_blank">Assistir agora</a>
+            </div>
+`
---
+++ aula4/dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Akatsuki no Yona",
+        avaliacao: "5 estrelas. Harem inverso, romance, fantasia, historico. Yona é uma personagem cativante e seus parceiros são apaixonantes. Apesar do genero, o anime ainda assim tem um romance sólido.",
+        link: "https://www.crunchyroll.com/pt-br/series/G6VN35J7R/yona-of-the-dawn"
+    },
+    {
+        titulo: "Haikyuu",
+        avaliacao: "5 estrelas. Esporte, slice of life. Envolvente e emocionante, depois de começar nã consegue largar. Impossível não se apegar a todos os personagens.",
+        link: "https://www.crunchyroll.com/pt-br/series/GY8VM8MWY/haikyu"
+    },
+    {
+        titulo: "Jujutsu Kaisen",
+        avaliacao: "5 estrelas. Ação, fantasia, terror leve. História extremamente envolvente, belos visuais, trama interessante. Anime main stream de alta qualidade.",
+        link: "https://www.crunchyroll.com/pt-br/series/GRDV0019R/jujutsu-kaisen"
+    }
+];
