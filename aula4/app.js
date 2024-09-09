7025bee2dc5bd37b935b7fe174a3f2bd3ed38074
Maria Eduarda Alves da Silva <maria.silva@bndes.gov.br>
2024-09-09 11:35:20 -0300

    Update: finaliza aula4

--- aula4/app.js
+++ aula4/app.js
@@ -1,16 +1,25 @@
-// console.log(dados);
-
-let section = document.getElementById("resultados-pesquisa");
-
-section.innerHTML = `
-    <div class="item-resultado">
-                <h2>
-                    <a href="#" target="_blank">Haikyuu</a>
-                </h2>
-                <p class="descricao-meta">
-                    5 estrelas. Esporte, slice of life. Envolvente e emocionante, depois de começar nã consegue largar. Impossível não se apegar a todos os personagens.
-                </p>
-                <a href="https://www.crunchyroll.com/pt-br/series/GY8VM8MWY/haikyu?srsltid=AfmBOorrtSSBRB_FT-7L0HR5SFt8-7vBjhSVlxnKfXmr4SMQvUMDo26Y"
-                    target="_blank">Assistir agora</a>
-            </div>
-`
+//faz aparecer os dados ao clicar em pesquisar
+function search() {
+    // Obtém a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+  
+    // Itera sobre cada elemento do array 'dados'
+    for (let dado of dados) {
+      // Constrói o HTML para cada resultado, incluindo título, avaliação e link
+      resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta"> ${dado.avaliacao}</p>
+          <a href=${dado.link}>Assistir agora</a>
+        </div>
+      `;
+    }
+  
+    // Atribui o HTML construído à seção de resultados
+    section.innerHTML = resultados; // Boas práticas: acessar section o mínimo possível
+  }
